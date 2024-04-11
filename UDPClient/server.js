953e08c359788cce6912cdfdd4d03a9164dffec4
Wesley Brandão <wesleybr.com@gmail.com>
2024-04-11 17:38:01 -0300

    seconde commit

--- UDPClient/server.js
+++ UDPClient/server.js
@@ -1,37 +1,62 @@
-var udp = require('dgram');
+var udp = require("dgram");
 
-var buffer = require('buffer');
+var buffer = require("buffer");
+
+var fs = require("fs");
 
 // creating a client socket
-var client = udp.createSocket('udp4');
+var client = udp.createSocket("udp4");
 
+const finalFile = {
+  data: "",
+  name: "",
+  hash: "",
+};
 //buffer msg
-var data = Buffer.from('image.png');
+var data = Buffer.from("image.png");
 
-client.on('message',function(msg,info){
-  console.log('Data received from server');
-  if(msg.toString().slice(0,3) == '404' ) {
-    console.log('Data received from server : ' + msg.toString());
+client.on("message", function (msg, info) {
+  console.log("Data received from server");
+  if (msg.toString().slice(0, 3) == "404") {
+    console.log("Data received from server : " + msg.toString());
+    console.log("dasd");
     return;
   }
 
-  console.log('Data received from server : ' + msg.toString());
+  if (finalFile.name == "") {
+    const t = msg.toString().split(":");
+    finalFile.name = t[0];
+    finalFile.hash = t[1];
+  } else {
+    finalFile.data += msg.toString();
+  }
+  console.log("Data received from server : " + msg.toString());
+  console.log(
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port,
+  );
 
-  /*
-  const file = msg.toString().split(':');
-  fs.writeFile(file[0], file[1], (err) => {
-    if(!err) console.log('Data written');
-  });
-  */
+  console.log(finalFile.name);
 
-  console.log('Data received from server : ' + msg.toString());
-  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+  if (msg.toString().endsWith("FINAL")) {
+    finalFile.data = finalFile.data.replace("FINAL", "");
+    createFile();
+  }
 });
 
-client.send(data,41234,'localhost',function(error){
-  if(error){
+function createFile() {
+  fs.writeFile(finalFile.name, finalFile.data, function (err) {
+    if (err) console.log("ERROR");
+    console.log("SALVO");
+  });
+}
+
+client.send(data, 41337, "localhost", function (error) {
+  if (error) {
     client.close();
-  }else{
-    console.log('Data sent !!!');
+  } else {
+    console.log("Data sent !!");
   }
 });
